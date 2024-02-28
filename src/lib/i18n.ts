84403e1ab5b7b56b8e19c59bf92923d36b4e2681
Artemy <artegoser@gmail.com>
2024-02-28 13:57:00 +0300

    add internationalization

---
+++ src/lib/i18n.ts
@@ -0,0 +1,16 @@
+import {
+  register,
+  init,
+  getLocaleFromNavigator,
+  waitLocale,
+} from "svelte-i18n";
+
+register("en", () => import("./locales/en.json"));
+register("ru", () => import("./locales/ru.json"));
+
+init({
+  fallbackLocale: "en",
+  initialLocale: getLocaleFromNavigator(),
+});
+
+await waitLocale();
