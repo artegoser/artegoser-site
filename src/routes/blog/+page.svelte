1dd4204153d3799dcd8297ea69ff0355db2451ab
Artemy <artegoser@gmail.com>
2024-02-28 18:49:23 +0300

    blog routing

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,1 @@
+<div class="container mx-auto flex justify-center items-center">Blog page</div>
---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div class="container mx-auto flex justify-center items-center">
+  Blog with id: {data.id}
+</div>
