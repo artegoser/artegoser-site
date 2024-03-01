57c744218eba4c6db8642f0abead6691e825b2cf
Artemy <artegoser@gmail.com>
2024-03-01 17:36:56 +0300

    fix anchors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
   <div class="strong-text uppercase">artegoser's site</div>
   <div>Soon there will be my portfolio</div>
   <div>
-    Now you can check my blog <a href="/blog" class="text-blue-600">here</a>
+    Now you can check my blog <a href="/blog" class="anchor">here</a>
   </div>
 </div>
