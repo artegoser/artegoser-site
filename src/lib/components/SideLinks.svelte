5471a4afd541a8af2ed46b65dda3aba0febd4cc7
Artemy <artegoser@gmail.com>
2024-03-04 17:45:37 +0300

    Update SideLinks.svelte

--- src/lib/components/SideLinks.svelte
+++ src/lib/components/SideLinks.svelte
@@ -1,12 +1,8 @@
 <div class="btn-group variant-ghost-surface">
-  <a
-    href="https://github.com/artegoser"
-    target="_blank"
-    rel="noreferrer"
-  >
+  <a href="https://github.com/artegoser" target="_blank" rel="noreferrer">
     GitHub
   </a>
-  <a href="https://t.me/artegosr" target="_blank" rel="noreferrer">
-    Telegram
+  <a href="https://t.me/artegoser_ru" target="_blank" rel="noreferrer">
+    Microblog (ru)
   </a>
 </div>
