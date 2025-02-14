346110bf9cc9302da1d06d6a3d54b745429de996
Artemy Egorov <artegoser@gmail.com>
2025-02-14 13:54:57 +0300

    rework?

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let text = 'ARTEGOSER';
+</script>
+
+<div class="header">
+	<img src="/logo.svg" alt="" class="w-16" />
+	<div class="flex flex-col gap-4">{text}</div>
+</div>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header text="PORTFOLIO" />
+
+<div class="mx-8 my-2 max-sm:mx-2">
+	<div class="flex w-full items-center justify-center">
+		<img src="/logo.svg" alt="" class="glow animate-float w-full" />
+		<div class="gap4 flex flex-col items-start justify-center">
+			<div class="text-6xl font-black max-sm:text-3xl">ARTEGOSER</div>
+			<div class="text-lg font-semibold text-zinc-400">Гений, миллиардер, плейбой, филантроп</div>
+		</div>
+	</div>
+</div>
